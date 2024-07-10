5da3336d4161bb55ca97f663f4bf6b27a14a1901
Ion <ion.baltaga@cote-azur.cci.fr>
2024-07-10 17:01:02 +0200

    Simplified Board component

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,12 +2,12 @@ import React, { useState } from 'react';
 import Square from './Square';
 import '../styles/Board.css';
 import { movePawn, movePawnBack } from './pieces/Pawn';
-import { moveToEmptySquare, findNextMovesPositions, emptyNextMoves} from '../utilities/Utilities';
-import { moveTower } from './pieces/Tower'
-import { moveHorse } from './pieces/Horse'
-import { moveBishop } from './pieces/Bishop'
-import { moveQueen } from './pieces/Queen'
-import { moveKing } from './pieces/King'
+import { moveToEmptySquare, findNextMovesPositions, emptyNextMoves } from '../utilities/Utilities';
+import { moveTower } from './pieces/Tower';
+import { moveHorse } from './pieces/Horse';
+import { moveBishop } from './pieces/Bishop';
+import { moveQueen } from './pieces/Queen';
+import { moveKing } from './pieces/King';
 
 const Board = () => {
     const initialBoard = [
@@ -20,99 +20,53 @@ const Board = () => {
         '♙', '♙', '♙', '♙', '♙', '♙', '♙', '♙',
         '♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖',
     ];
+
     const [pieces, setPieces] = useState(initialBoard);
     const [valeur, setValeur] = useState();
     const [greenSquares, setGreenSquares] = useState([]);
     const [whiteToMove, setWhiteToMove] = useState(true);
-    const isWhite = true;
+
+    const handleMove = (index, moveFunction, isWhite) => {
+        moveFunction(pieces, index, setPieces, setValeur, setGreenSquares, isWhite);
+    };
 
     const handleSquareClick = (index) => {
-        console.log("white moves : " + whiteToMove);
-        if (whiteToMove) {
-            if (pieces[index] !== '') {
-                //Empty square
-                if (pieces[index] === "\u25CB" || greenSquares.includes(index)) {
-                    moveToEmptySquare(pieces, index, valeur, setPieces, setGreenSquares);
-                    setWhiteToMove(!whiteToMove);
-                }
-                //White pieces
-                else if (pieces[index] === '♙') {
-                    movePawnBack(pieces, index, setPieces, setValeur, setGreenSquares);
-                }
-                else if (pieces[index] === '♖') {
-                    moveTower(pieces, index, setPieces, setValeur, setGreenSquares, isWhite);
-                }
-                else if (pieces[index] === '♘') {
-                    moveHorse(pieces, index, setPieces, setValeur, setGreenSquares, isWhite);
-                }
-                else if (pieces[index] === '♗') {
-                    moveBishop(pieces, index, setPieces, setValeur, setGreenSquares, isWhite);
-                }
-                else if (pieces[index] === '♕') {
-                    moveQueen(pieces, index, setPieces, setValeur, setGreenSquares, isWhite);
-                }
-                else if (pieces[index] === '♔') {
-                    moveKing(pieces, index, setPieces, setValeur, setGreenSquares, isWhite);
-                }
+        if (pieces[index] !== '') {
+            if (pieces[index] === "\u25CB" || greenSquares.includes(index)) {
+                moveToEmptySquare(pieces, index, valeur, setPieces, setGreenSquares);
+                setWhiteToMove(!whiteToMove);
+            } else if (whiteToMove && '♙♖♘♗♕♔'.includes(pieces[index])) {
+                handleMove(index, getMoveFunction(pieces[index]), true);
+            } else if (!whiteToMove && '♟︎♜♞♝♛♚'.includes(pieces[index])) {
+                handleMove(index, getMoveFunction(pieces[index]), false);
             }
-            else {
+        } else {
+            const nextMovesPositions = findNextMovesPositions(pieces);
+            if (nextMovesPositions !== null) {
                 const newGreenSquares = [];
                 setGreenSquares(newGreenSquares);
-
-                const nextMovesPositions = findNextMovesPositions(pieces);
-                if (nextMovesPositions === null) {
-                    return;
-                }
                 const getNewPieces = emptyNextMoves(pieces, nextMovesPositions, setGreenSquares);
                 setPieces(getNewPieces);
             }
         }
-        else {
-            if (pieces[index] !== '') {
-                //Empty square
-                if (pieces[index] === "\u25CB" || greenSquares.includes(index)) {
-                    moveToEmptySquare(pieces, index, valeur, setPieces, setGreenSquares);
-                    setWhiteToMove(!whiteToMove);
-                }
-                //Black pieces
-                else if (pieces[index] === '♟︎') {
-                    movePawn(pieces, index, setPieces, setValeur, setGreenSquares);
-                }
-                else if (pieces[index] === '♜') {
-                    moveTower(pieces, index, setPieces, setValeur, setGreenSquares, !isWhite);
-                }
-                else if (pieces[index] === '♞') {
-                    moveHorse(pieces, index, setPieces, setValeur, setGreenSquares, !isWhite);
-                }
-                else if (pieces[index] === '♝') {
-                    moveBishop(pieces, index, setPieces, setValeur, setGreenSquares, !isWhite);
-                }
-                else if (pieces[index] === '♛') {
-                    moveQueen(pieces, index, setPieces, setValeur, setGreenSquares, !isWhite);
-                }
-                else if (pieces[index] === '♚') {
-                    moveKing(pieces, index, setPieces, setValeur, setGreenSquares, !isWhite);
-                }
-            }
-            else {
-                const newGreenSquares = [];
-                setGreenSquares(newGreenSquares);
+    };
 
-                const nextMovesPositions = findNextMovesPositions(pieces);
-                if (nextMovesPositions === null) {
-                    return;
-                }
-                const getNewPieces = emptyNextMoves(pieces, nextMovesPositions, setGreenSquares);
-                setPieces(getNewPieces);
-            }
+    const getMoveFunction = (piece) => {
+        switch (piece) {
+            case '♙': return movePawnBack;
+            case '♟︎': return movePawn;
+            case '♖': case '♜': return moveTower;
+            case '♘': case '♞': return moveHorse;
+            case '♗': case '♝': return moveBishop;
+            case '♕': case '♛': return moveQueen;
+            case '♔': case '♚': return moveKing;
+            default: return () => {};
         }
-        
     };
 
     const renderSquare = (i) => {
         const piece = pieces[i];
         const isGreenSquare = greenSquares.includes(i);
-        // console.log("isGreenSquare : " + isGreenSquare);
         return (
             <Square
                 key={i}
